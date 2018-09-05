a86c48f206304a114611004573fb0e6919be6c5f
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-05 18:01:58 -0400

    Brew formula update for release version v1.0.15

--- release.rb
+++ release.rb
@@ -1,9 +1,9 @@
 class Release < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/release/releases/download/v1.0.14/release_1.0.14_darwin_amd64.tar.gz"
-  version "1.0.14"
-  sha256 "aa43ec2c1f182c591b5c98ebd84b5023dc00bbbc55c154af020e01104efe2e11"
+  url "https://github.com/gobuffalo/release/releases/download/v1.0.15/release_1.0.15_darwin_amd64.tar.gz"
+  version "1.0.15"
+  sha256 "ff89765323a63fc5f47ed9ce7ff86f1d5cf4013d7d3d23d4547ab53e21ecd5da"
 
   def install
     bin.install "release"
