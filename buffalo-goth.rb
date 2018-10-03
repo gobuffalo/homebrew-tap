e40bf820c9a579d22306be569ebeb401e3aa31cb
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-03 13:25:21 -0400

    Brew formula update for buffalo-goth version v1.0.2

--- buffalo-goth.rb
+++ buffalo-goth.rb
@@ -1,9 +1,9 @@
 class BuffaloGoth < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/buffalo-goth/releases/download/v1.0.1/buffalo-goth_1.0.1_darwin_amd64.tar.gz"
-  version "1.0.1"
-  sha256 "9c6ce7103ced3086aec266870ff3b8ac4f275965af3e8d2e3dbbac33bcbf0c2f"
+  url "https://github.com/gobuffalo/buffalo-goth/releases/download/v1.0.2/buffalo-goth_1.0.2_darwin_amd64.tar.gz"
+  version "1.0.2"
+  sha256 "1f37b3a355168f09874a5fe165818e07b51472cffff62c7914d45caeb72c52d2"
 
   def install
     bin.install "buffalo-goth"
