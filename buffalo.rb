548463ff53f1eeac73100a35670b533275476bc6
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-14 16:31:34 -0400

    Brew formula update for buffalo version v0.12.8

--- buffalo.rb
+++ buffalo.rb
@@ -1,9 +1,9 @@
 class Buffalo < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/buffalo/releases/download/v0.12.7/buffalo_0.12.7_darwin_amd64.tar.gz"
-  version "0.12.7"
-  sha256 "7bb2fa62adc00bceb800acd8e611d158ad325ed7a49d1e2f6b31e1c02f627223"
+  url "https://github.com/gobuffalo/buffalo/releases/download/v0.12.8/buffalo_0.12.8_darwin_amd64.tar.gz"
+  version "0.12.8"
+  sha256 "1dfb90a7af118c03dda893c9373dd671efb79ab3bcbbd07dc0f2b777c0dcf5f4"
 
   def install
     bin.install "buffalo"
