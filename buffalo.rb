88563333dfae70961ba27657fde78b5cf8329169
Mark Bates <mark@markbates.com>
2018-10-26 09:00:13 -0400

    Update buffalo.rb

--- buffalo.rb
+++ buffalo.rb
@@ -1,9 +1,9 @@
 class Buffalo < Formula
   desc "A Go web development eco-system, designed to make your life easier."
   homepage "https://gobuffalo.io"
-  url "https://github.com/gobuffalo/buffalo/releases/download/v0.13.3-alpha.1/buffalo_0.13.3-alpha.1_darwin_amd64.tar.gz"
-  version "0.13.3-alpha.1"
-  sha256 "6dadb5b0477eb92d1389742c3e905c1d2f6885ae1ff2f4788d31a5e4f2949ec8"
+  url "https://github.com/gobuffalo/buffalo/releases/download/v0.13.2/buffalo_0.13.2_darwin_amd64.tar.gz"
+  version "0.13.2"
+  sha256 "264d811a65bbca7111d4de47e0f5b192a9481e001590f868a8d942a7fef883a9"
 
   def install
     bin.install "buffalo-no-sqlite"
