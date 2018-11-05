d9fb34c96dd385edca6d208a4e3b799ce778f70c
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-05 15:13:53 -0500

    Brew formula update for buffalo-docker version v1.0.5

--- buffalo-docker.rb
+++ buffalo-docker.rb
@@ -1,9 +1,9 @@
 class BuffaloDocker < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/buffalo-docker/releases/download/v1.0.4/buffalo-docker_1.0.4_darwin_amd64.tar.gz"
-  version "1.0.4"
-  sha256 "f724350949cb882a8db550cf3142f302f2e2ef34d779c26a2bf6db26f38e6d5c"
+  url "https://github.com/gobuffalo/buffalo-docker/releases/download/v1.0.5/buffalo-docker_1.0.5_darwin_amd64.tar.gz"
+  version "1.0.5"
+  sha256 "3d3f7302c391ce3835b86d4b483739f3407f8dc511319d50c825ecff9c8e6c37"
 
   def install
     bin.install "buffalo-docker"
