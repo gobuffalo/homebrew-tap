d71b3e8e0e6ce77796a6f82d08764a50e4e94c25
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-14 17:00:49 -0500

    Brew formula update for packr version v1.21.0

--- packr.rb
+++ packr.rb
@@ -1,9 +1,9 @@
 class Packr < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/packr/releases/download/v1.20.0/packr_1.20.0_darwin_amd64.tar.gz"
-  version "1.20.0"
-  sha256 "20efd3f4926e59da88052bbda832d8a5c527fa9a786643134ac38d142fd89797"
+  url "https://github.com/gobuffalo/packr/releases/download/v1.21.0/packr_1.21.0_darwin_amd64.tar.gz"
+  version "1.21.0"
+  sha256 "c3102b447a00d4c5de29fb0b135970e9ef2bbefac0c14dbc8c0c9037a0ed3926"
 
   def install
     bin.install "packr"
