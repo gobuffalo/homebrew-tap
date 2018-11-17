ca5b1e9f28c5b34206721630435b67957aabe21c
goreleaserbot <goreleaser@carlosbecker.com>
2018-11-17 12:15:44 -0500

    Brew formula update for buffalo-pop version v1.1.15

--- buffalo-pop.rb
+++ buffalo-pop.rb
@@ -1,9 +1,9 @@
 class BuffaloPop < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/buffalo-pop/releases/download/v1.1.14/buffalo-pop_1.1.14_darwin_amd64.tar.gz"
-  version "1.1.14"
-  sha256 "2bbad0056a4efb25880cfcb17c83e0efefc2e54f0dad08916b5b5e30108b65d5"
+  url "https://github.com/gobuffalo/buffalo-pop/releases/download/v1.1.15/buffalo-pop_1.1.15_darwin_amd64.tar.gz"
+  version "1.1.15"
+  sha256 "5d5ae0299b9e1a1dc3258ea53f3da6d890287fcc91354c035f9b84e6d1f45747"
 
   def install
     bin.install "buffalo-pop"
