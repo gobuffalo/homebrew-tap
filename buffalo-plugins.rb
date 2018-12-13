40f21be3798bcc61383b5be27a886c58fb6f99cc
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-13 13:45:36 -0500

    Brew formula update for buffalo-plugins version v1.9.1

--- buffalo-plugins.rb
+++ buffalo-plugins.rb
@@ -1,9 +1,9 @@
 class BuffaloPlugins < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/buffalo-plugins/releases/download/v1.9.0/buffalo-plugins_1.9.0_darwin_amd64.tar.gz"
-  version "1.9.0"
-  sha256 "2ba93023e68c75276371edfaf6f6bccdbbb545b72510f23b18765197ee71dc05"
+  url "https://github.com/gobuffalo/buffalo-plugins/releases/download/v1.9.1/buffalo-plugins_1.9.1_darwin_amd64.tar.gz"
+  version "1.9.1"
+  sha256 "3e33036b792668d68b78c5cbc57de27e460e196e7188e680ad2acff4c5dfa37e"
 
   def install
     bin.install "buffalo-plugins"
