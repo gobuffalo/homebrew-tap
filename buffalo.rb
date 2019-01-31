1d94c320ccf6d186976d247d209ae37a9e4bec0d
goreleaserbot <goreleaser@carlosbecker.com>
2019-01-31 11:10:58 -0500

    Brew formula update for buffalo version v0.13.13

--- buffalo.rb
+++ buffalo.rb
@@ -1,9 +1,9 @@
 class Buffalo < Formula
   desc "A Go web development eco-system, designed to make your life easier."
   homepage "https://gobuffalo.io"
-  url "https://github.com/gobuffalo/buffalo/releases/download/v0.13.12/buffalo_0.13.12_darwin_amd64.tar.gz"
-  version "0.13.12"
-  sha256 "b76582a12cc1e4e84bd248a189b1efeb73502a9f16f673c9782c427233ac729a"
+  url "https://github.com/gobuffalo/buffalo/releases/download/v0.13.13/buffalo_0.13.13_darwin_amd64.tar.gz"
+  version "0.13.13"
+  sha256 "c5c69ffb6aece056090c52440c5f357834f1c459e95d7444cd97783b930de256"
 
   def install
     bin.install "buffalo-no-sqlite"
