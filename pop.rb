74695ef09b230a4f80616df4325f61a00ccfab7b
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-15 22:52:45 +0100

    Brew formula update for pop version v4.9.8

--- pop.rb
+++ pop.rb
@@ -1,9 +1,9 @@
 class Pop < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/pop/releases/download/v4.9.7/pop_4.9.7_darwin_amd64.tar.gz"
-  version "4.9.7"
-  sha256 "0c322a69d8cee75e2b743ebf8ea1bde40590f77b11cf72e79449d3130a1030d0"
+  url "https://github.com/gobuffalo/pop/releases/download/v4.9.8/pop_4.9.8_darwin_amd64.tar.gz"
+  version "4.9.8"
+  sha256 "d62b67b0816c996ae3b23e196b415a324e2387643ef8b289ba1679192dbebb6b"
 
   def install
     bin.install "soda"
