c882d676485ffe2d953693f8d5a08ba1a830e441
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-25 10:02:59 -0500

    Brew formula update for buffalo-auth version v1.0.5

--- buffalo-auth.rb
+++ buffalo-auth.rb
@@ -1,9 +1,9 @@
 class BuffaloAuth < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.0.4/buffalo-auth_1.0.4_darwin_amd64.tar.gz"
-  version "1.0.4"
-  sha256 "bc4d93628d47e292df527e1dd6a6098cc48cc01fefc19ad3aca5a8afb15cc560"
+  url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.0.5/buffalo-auth_1.0.5_darwin_amd64.tar.gz"
+  version "1.0.5"
+  sha256 "4fee8c813976537bbffc8d0d14d1cc4d4ef794ca501870bc20332597560f4bd5"
 
   def install
     bin.install "buffalo-auth"
