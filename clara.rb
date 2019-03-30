a76eac6eb45fa5e65c616d3aaae467a6e08c6202
goreleaserbot <goreleaser@carlosbecker.com>
2019-03-30 15:30:55 -0400

    Brew formula update for clara version v0.4.0

--- clara.rb
+++ clara.rb
@@ -1,9 +1,9 @@
 class Clara < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/clara/releases/download/v0.3.4/clara_0.3.4_darwin_amd64.tar.gz"
-  version "0.3.4"
-  sha256 "054c9767dba980f49a75dca6c5150d7b0bf8ce5c792ef4d151ee9640cdcef079"
+  url "https://github.com/gobuffalo/clara/releases/download/v0.4.0/clara_0.4.0_darwin_amd64.tar.gz"
+  version "0.4.0"
+  sha256 "4461496173f7ccd9d8fa33d5ffa4c282aa7e81f74f45c60d1b7badbe3b9fc289"
 
   def install
     bin.install "clara"
