462065f098f9e80182b98f2dddfc1133ad7f6667
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-16 13:28:46 -0700

    Brew formula update for plush version v3.8.0

--- plush.rb
+++ plush.rb
@@ -1,9 +1,9 @@
 class Plush < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/plush/releases/download/v3.7.33/plush_3.7.33_darwin_amd64.tar.gz"
-  version "3.7.33"
-  sha256 "7693a6e85369446be68207043b7cbc9cf79adf146889c29a9e7eb96848948db7"
+  url "https://github.com/gobuffalo/plush/releases/download/v3.8.0/plush_3.8.0_darwin_amd64.tar.gz"
+  version "3.8.0"
+  sha256 "191b5e16b537cf84810e238ca3063321b101352f1c4ccf3a01edd732682aaae5"
 
   def install
     bin.install "plush"
