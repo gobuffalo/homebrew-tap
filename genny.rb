180138b4eeb7907caa284ddbd5f5904b220fad69
goreleaserbot <goreleaser@carlosbecker.com>
2019-04-22 12:27:29 -0400

    Brew formula update for genny version v0.1.1

--- genny.rb
+++ genny.rb
@@ -1,9 +1,9 @@
 class Genny < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/genny/releases/download/v0.1.0/genny_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "7ebb0fd7bb9cc22c51d62cc9a6a88be9c080dd2a3f7a5756734468b07b078bc4"
+  url "https://github.com/gobuffalo/genny/releases/download/v0.1.1/genny_0.1.1_darwin_amd64.tar.gz"
+  version "0.1.1"
+  sha256 "33d645ebbc3bb6f8e86abe95b480c41546761e4b2e418b3371fa70273c27bf6b"
 
   def install
     bin.install "genny"
