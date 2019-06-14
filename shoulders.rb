28994c3069b8a0069f46685a025877d2d9a8e7d2
goreleaserbot <goreleaser@carlosbecker.com>
2019-06-14 11:03:07 -0500

    Brew formula update for shoulders version v1.1.0

--- shoulders.rb
+++ shoulders.rb
@@ -1,9 +1,9 @@
 class Shoulders < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/shoulders/releases/download/v1.0.3/shoulders_1.0.3_darwin_amd64.tar.gz"
-  version "1.0.3"
-  sha256 "bfb51e921ea39ce5c37a0ee7e29e40abcba27d34eaeaf6df59ee7029038d5061"
+  url "https://github.com/gobuffalo/shoulders/releases/download/v1.1.0/shoulders_1.1.0_darwin_amd64.tar.gz"
+  version "1.1.0"
+  sha256 "6895c240bfbe638694986fa3f08c9379ef4c5bd1fab538d03bf2b9d76cd98c01"
 
   def install
     bin.install "shoulders"
