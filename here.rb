83bb0754452b250ff02890150934bb9ea9a6a6b1
goreleaserbot <goreleaser@carlosbecker.com>
2019-06-24 10:57:13 -0500

    Brew formula update for here version v0.1.5

--- here.rb
+++ here.rb
@@ -1,9 +1,9 @@
 class Here < Formula
   desc ""
   homepage ""
-  url "https://github.com/gobuffalo/here/releases/download/v0.1.2/here_0.1.2_darwin_amd64.tar.gz"
-  version "0.1.2"
-  sha256 "6aa88ada5014fed6b22d9ef0cd6656e2293d4e580c0a3d5f6d28725c084d1a41"
+  url "https://github.com/gobuffalo/here/releases/download/v0.1.5/here_0.1.5_darwin_amd64.tar.gz"
+  version "0.1.5"
+  sha256 "da5d156430b6bc4d59b4ef37288d4452bb99881898b552262a7629417bd76758"
 
   def install
     bin.install "here"
