b2baf964a2083fc2d5d6a27f4de7642d2b6b831c
goreleaserbot <goreleaser@carlosbecker.com>
2019-08-26 04:09:12 +0100

    Brew formula update for buffalo version v0.14.10

--- buffalo.rb
+++ buffalo.rb
@@ -2,16 +2,16 @@
 class Buffalo < Formula
   desc "A Go web development eco-system, designed to make your life easier."
   homepage "https://gobuffalo.io"
-  version "0.14.9"
+  version "0.14.10"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gobuffalo/buffalo/releases/download/v0.14.9/buffalo_0.14.9_darwin_amd64.tar.gz"
-    sha256 "0aadd0081e2089253ed198f9714e24cbd559fd2e4b00f4b27964dd8ae969f318"
+    url "https://github.com/gobuffalo/buffalo/releases/download/v0.14.10/buffalo_0.14.10_darwin_amd64.tar.gz"
+    sha256 "8b965c84f1b10ce4baca3b0a41970eaa7a424a62d13b8d133f767bf6f7dea605"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gobuffalo/buffalo/releases/download/v0.14.9/buffalo_0.14.9_linux_amd64.tar.gz"
-      sha256 "5c148a7b84ac1f93a83811bb16d83cff6878da418d9e0ef34db95fb263668667"
+      url "https://github.com/gobuffalo/buffalo/releases/download/v0.14.10/buffalo_0.14.10_linux_amd64.tar.gz"
+      sha256 "5305c90801a2520ac71a5205304c68cea6ce0f4a197dbb89e362a6e02cf0c6ca"
     end
   end
 
