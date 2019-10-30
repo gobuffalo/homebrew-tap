b33bab160fe37b02976f324513286ea5eb425567
goreleaserbot <goreleaser@carlosbecker.com>
2019-10-30 08:40:00 +0100

    Brew formula update for buffalo-pop version v1.23.1

--- buffalo-pop.rb
+++ buffalo-pop.rb
@@ -2,14 +2,14 @@
 class BuffaloPop < Formula
   desc "Buffalo plugin to work with gobuffalo/pop"
   homepage ""
-  version "1.22.0"
+  version "1.23.1"
 
   if OS.mac?
-    url "https://github.com/gobuffalo/buffalo-pop/releases/download/v1.22.0/buffalo-pop_1.22.0_darwin_amd64.tar.gz"
-    sha256 "101b3aead18d2b3522c7acc7359881068a1530d8c46137e98280b9eba805dae2"
+    url "https://github.com/gobuffalo/buffalo-pop/releases/download/v1.23.1/buffalo-pop_1.23.1_darwin_amd64.tar.gz"
+    sha256 "a8e53d2f8972ae8e05f8bfcb5e93c8bc3cf5e374e05a19935d07b38ece00dfe9"
   elsif OS.linux?
-    url "https://github.com/gobuffalo/buffalo-pop/releases/download/v1.22.0/buffalo-pop_1.22.0_linux_amd64.tar.gz"
-    sha256 "6200acbc9f79a61a1f08b25cda8626200f0d4f82f2349d119e93f910b54f51a5"
+    url "https://github.com/gobuffalo/buffalo-pop/releases/download/v1.23.1/buffalo-pop_1.23.1_linux_amd64.tar.gz"
+    sha256 "e1df6567caf79485b7609516cc456346029d1677f72d1269737a4211df6043b6"
   end
 
   def install
