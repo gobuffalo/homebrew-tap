ec62a3ce1b56c7ac5c8eac2b2382a3e6972c0204
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-24 09:33:24 -0500

    Brew formula update for genny version v0.6.0

--- genny.rb
+++ genny.rb
@@ -2,24 +2,24 @@
 class Genny < Formula
   desc ""
   homepage ""
-  version "0.5.2"
+  version "0.6.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gobuffalo/genny/releases/download/v0.5.2/genny_0.5.2_Darwin_x86_64.tar.gz"
-    sha256 "f641553110f89f89c7e00c4f40b239e4830b0a69db9c84b8c862969dc4337198"
+    url "https://github.com/gobuffalo/genny/releases/download/v0.6.0/genny_0.6.0_Darwin_x86_64.tar.gz"
+    sha256 "4822b9e969c95dcda4a0930803ac611a04e27affc653249abc51206948f2d32b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gobuffalo/genny/releases/download/v0.5.2/genny_0.5.2_Linux_x86_64.tar.gz"
-      sha256 "7b1f8133ee02729495b7c92dee94341181f78e9c478c871478e6282ed8ad9075"
+      url "https://github.com/gobuffalo/genny/releases/download/v0.6.0/genny_0.6.0_Linux_x86_64.tar.gz"
+      sha256 "63b0f0b6770b2aaa126e79f2c3df546b78dc21f5ff919abfc3ee27b9cae4c489"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/gobuffalo/genny/releases/download/v0.5.2/genny_0.5.2_Linux_arm64.tar.gz"
-        sha256 "3314a2514db1397319ddf630a76aa65df7cd83baa736f45d9563d92909e9f92a"
+        url "https://github.com/gobuffalo/genny/releases/download/v0.6.0/genny_0.6.0_Linux_arm64.tar.gz"
+        sha256 "754f5e3f0d04d341fa84104d661f85c0428149b0791498f3d1abecbb9be18aef"
       else
-        url "https://github.com/gobuffalo/genny/releases/download/v0.5.2/genny_0.5.2_Linux_armv6.tar.gz"
-        sha256 "a38752204a7d5ec6b39f0165f3a164fbf1a28bf36fe73fa0d9f77d9f3bb1a070"
+        url "https://github.com/gobuffalo/genny/releases/download/v0.6.0/genny_0.6.0_Linux_armv6.tar.gz"
+        sha256 "2572d5880e792a5f2876f1b56c8f28031cd56ea5e41594919144de0c3f5c0b85"
       end
     end
   end
