bc7e150554582048ac0bbd2f5d30af1cc2e44c6d
goreleaserbot <goreleaser@carlosbecker.com>
2020-01-31 13:12:44 -0500

    Brew formula update for meta version v2.0.1

--- meta.rb
+++ meta.rb
@@ -2,24 +2,24 @@
 class Meta < Formula
   desc ""
   homepage ""
-  version "2.0.0"
+  version "2.0.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gobuffalo/meta/releases/download/v2.0.0/meta_2.0.0_Darwin_x86_64.tar.gz"
-    sha256 "4b8534c9bb374645a07545ff611809255faa3c991c63d24b93f5493b1539b7f8"
+    url "https://github.com/gobuffalo/meta/releases/download/v2.0.1/meta_2.0.1_Darwin_x86_64.tar.gz"
+    sha256 "86fe3049b01d862c844e5fb4a5ffec61d5f710f907daba5a04d28d8f1edbf8d2"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gobuffalo/meta/releases/download/v2.0.0/meta_2.0.0_Linux_x86_64.tar.gz"
-      sha256 "fa029971ab3a371244ec48752667f51c6b8f3e778267c1fc139c4fcd19c1f75a"
+      url "https://github.com/gobuffalo/meta/releases/download/v2.0.1/meta_2.0.1_Linux_x86_64.tar.gz"
+      sha256 "caa564dd064b0ad6f8151774f1331e2cdb90630fb8b8d80e18750d39010cb3b5"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/gobuffalo/meta/releases/download/v2.0.0/meta_2.0.0_Linux_arm64.tar.gz"
-        sha256 "77a6f2203480c66e0c99cc552f91958ff72308d4ffbc4dd594602e7ebb450d02"
+        url "https://github.com/gobuffalo/meta/releases/download/v2.0.1/meta_2.0.1_Linux_arm64.tar.gz"
+        sha256 "943c76f15396ed89cd367712e474e919414a181dcd69f28e5b04d7f276e3371e"
       else
-        url "https://github.com/gobuffalo/meta/releases/download/v2.0.0/meta_2.0.0_Linux_armv6.tar.gz"
-        sha256 "c853fb11da070109d58a66c3f0d3d55d6f2c64cd4b9429de3761c2c6ff996d5d"
+        url "https://github.com/gobuffalo/meta/releases/download/v2.0.1/meta_2.0.1_Linux_armv6.tar.gz"
+        sha256 "5e3f8d6e4987a2092734a8d4d7775f903f4366c4a6697eea162cacc0454f3b5b"
       end
     end
   end
