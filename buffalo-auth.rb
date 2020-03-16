baa3aadcc4803a1be099f85803c3e0e34019ea44
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-16 15:22:09 -0400

    Brew formula update for buffalo-auth version v1.3.1

--- buffalo-auth.rb
+++ buffalo-auth.rb
@@ -2,24 +2,24 @@
 class BuffaloAuth < Formula
   desc ""
   homepage ""
-  version "1.3.0"
+  version "1.3.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.3.0/buffalo-auth_1.3.0_Darwin_x86_64.tar.gz"
-    sha256 "0e5873e7e0655f6d4b4a7294e9659435b2a55c9b2469ce82f5afc74c1784c0e8"
+    url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.3.1/buffalo-auth_1.3.1_Darwin_x86_64.tar.gz"
+    sha256 "7ae104a99e21ce04de54e3c0096c8773cede4e423c00f7ee67fd5ae5e344e444"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.3.0/buffalo-auth_1.3.0_Linux_x86_64.tar.gz"
-      sha256 "7e9b8104e8050767d48bd397494aaccaae5a8f5daff365ff53f268a23ad94f04"
+      url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.3.1/buffalo-auth_1.3.1_Linux_x86_64.tar.gz"
+      sha256 "f3c790929a1a9b4be859335226e427bacb44018917df7964ad200220212f92d9"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.3.0/buffalo-auth_1.3.0_Linux_arm64.tar.gz"
-        sha256 "e646a950067312b2b122da4111cc3d5aa818e2e4c8e3a469b79de6ff64d96cc7"
+        url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.3.1/buffalo-auth_1.3.1_Linux_arm64.tar.gz"
+        sha256 "aba9c63aa9c303ba3bcc21e8fa04761b47846a1ff40aa3de0fc284036f8802c8"
       else
-        url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.3.0/buffalo-auth_1.3.0_Linux_armv6.tar.gz"
-        sha256 "a31c45277f46a91b1c5042109005efddc5169355b9c096db25d30253d2a00cf4"
+        url "https://github.com/gobuffalo/buffalo-auth/releases/download/v1.3.1/buffalo-auth_1.3.1_Linux_armv6.tar.gz"
+        sha256 "eaebb4747ebf02390be63d9290d9f5bbfe837c7ba16e32be80740408ce14dfdb"
       end
     end
   end
