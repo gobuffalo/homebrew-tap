93bacfbdc1aae593962396af95f2eeac00f87bcd
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-25 09:16:58 +0200

    Brew formula update for pop version v5.1.1

--- pop.rb
+++ pop.rb
@@ -2,16 +2,16 @@
 class Pop < Formula
   desc "A Tasty Treat For All Your Database Needs"
   homepage "https://gobuffalo.io/docs/db/getting-started"
-  version "5.0.11"
+  version "5.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gobuffalo/pop/releases/download/v5.0.11/pop_5.0.11_darwin_amd64.tar.gz"
-    sha256 "7051bf7c0b274bc93be958b61ad24affb455186f1b31f0b800f7145643c57f03"
+    url "https://github.com/gobuffalo/pop/releases/download/v5.1.1/pop_5.1.1_darwin_amd64.tar.gz"
+    sha256 "0b62b646a677673d7dc5b7a1eaf1fa6c2a8f6f16583e5be2231ac4b31c958b77"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gobuffalo/pop/releases/download/v5.0.11/pop_5.0.11_linux_amd64.tar.gz"
-      sha256 "7f447c2f136ca611717ac635cc4112d12e0d5764e4ba7b5502a364dcb76080a4"
+      url "https://github.com/gobuffalo/pop/releases/download/v5.1.1/pop_5.1.1_linux_amd64.tar.gz"
+      sha256 "cd69e5561b1dc6120f384e456a62598e1bfaa34dbc0ccb9a1b25ed2b52cf3883"
     end
   end
 
