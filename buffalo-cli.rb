8dc4c54a980dcc8b71536fc674f7eb4eaf684969
goreleaserbot <goreleaser@carlosbecker.com>
2020-04-26 17:58:15 -0400

    Brew formula update for buffalo-cli version v2.0.0-alpha.12

--- buffalo-cli.rb
+++ buffalo-cli.rb
@@ -2,24 +2,24 @@
 class BuffaloCli < Formula
   desc ""
   homepage ""
-  version "2.0.0-alpha.11"
+  version "2.0.0-alpha.12"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gobuffalo/buffalo-cli/releases/download/v2.0.0-alpha.11/buffalo-cli_2.0.0-alpha.11_Darwin_x86_64.tar.gz"
-    sha256 "b18b31fb532098f5cbd01b242cae6e67ff7ebbbb8fe4453d678d4960d5d50d1e"
+    url "https://github.com/gobuffalo/buffalo-cli/releases/download/v2.0.0-alpha.12/buffalo-cli_2.0.0-alpha.12_Darwin_x86_64.tar.gz"
+    sha256 "5c1982e3b26b4fb91b98082b21138875f08b31d35073815ecd4a0133fe6d063a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gobuffalo/buffalo-cli/releases/download/v2.0.0-alpha.11/buffalo-cli_2.0.0-alpha.11_Linux_x86_64.tar.gz"
-      sha256 "280226bf092c3c27b4c6086bb00490e1707450238c356fa4c0ca54e970f9d0ad"
+      url "https://github.com/gobuffalo/buffalo-cli/releases/download/v2.0.0-alpha.12/buffalo-cli_2.0.0-alpha.12_Linux_x86_64.tar.gz"
+      sha256 "0784b89da53a413e8757b84304dbb28e0e7d119f8451f163e167ea0187dd53e7"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/gobuffalo/buffalo-cli/releases/download/v2.0.0-alpha.11/buffalo-cli_2.0.0-alpha.11_Linux_arm64.tar.gz"
-        sha256 "92e3f8158f615ea1b4665f6dc835d3a3d55968a6082421c71309f86969529da1"
+        url "https://github.com/gobuffalo/buffalo-cli/releases/download/v2.0.0-alpha.12/buffalo-cli_2.0.0-alpha.12_Linux_arm64.tar.gz"
+        sha256 "6e1e4d001040eb2331d36018262675d8f7795b4b7a2232200f875a84ca33afad"
       else
-        url "https://github.com/gobuffalo/buffalo-cli/releases/download/v2.0.0-alpha.11/buffalo-cli_2.0.0-alpha.11_Linux_armv6.tar.gz"
-        sha256 "b1217c0c71649c6fc9c26dbb1c8fcea0f4756494e4d57dc259a842b8245b4982"
+        url "https://github.com/gobuffalo/buffalo-cli/releases/download/v2.0.0-alpha.12/buffalo-cli_2.0.0-alpha.12_Linux_armv6.tar.gz"
+        sha256 "8c9e5b1c203d17ebbcb5912c734b41d64d1115982b5545c0cee8de8ad8314514"
       end
     end
   end
