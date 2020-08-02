6afe389567c919d9e296274240831414daae7307
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-02 11:18:13 -0400

    Brew formula update for buffalo version v0.16.14

--- buffalo.rb
+++ buffalo.rb
@@ -2,24 +2,24 @@
 class Buffalo < Formula
   desc ""
   homepage ""
-  version "0.16.13"
+  version "0.16.14"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gobuffalo/buffalo/releases/download/v0.16.13/buffalo_0.16.13_Darwin_x86_64.tar.gz"
-    sha256 "35db7368532620951924845c307f2251b2f6d00481169c9c319741360d655bfe"
+    url "https://github.com/gobuffalo/buffalo/releases/download/v0.16.14/buffalo_0.16.14_Darwin_x86_64.tar.gz"
+    sha256 "7aa5ecaea401f50b2811dcfee874f16e4f5fbb8c0823df4631f6132481b54248"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gobuffalo/buffalo/releases/download/v0.16.13/buffalo_0.16.13_Linux_x86_64.tar.gz"
-      sha256 "81705d7862b8dc0d2858de54ce3b87ac181504d8c0e46ab9796b0cdfa4753505"
+      url "https://github.com/gobuffalo/buffalo/releases/download/v0.16.14/buffalo_0.16.14_Linux_x86_64.tar.gz"
+      sha256 "6d99db32c848ccfd4370af782781ba399bd257535ba54396bac8fa7c90c825cc"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/gobuffalo/buffalo/releases/download/v0.16.13/buffalo_0.16.13_Linux_arm64.tar.gz"
-        sha256 "b66286e7d8e98df280764e8558c68b104ca2243b4446df15525bfd960871d965"
+        url "https://github.com/gobuffalo/buffalo/releases/download/v0.16.14/buffalo_0.16.14_Linux_arm64.tar.gz"
+        sha256 "fffb7830e1d7278e8160e11caf3009ab98ce6b717b1d0a4ff69037cf64ffec74"
       else
-        url "https://github.com/gobuffalo/buffalo/releases/download/v0.16.13/buffalo_0.16.13_Linux_armv6.tar.gz"
-        sha256 "8b1177121c6b978a1d0f5e78b30738ecba24e6f72edd07126f5de52647adb041"
+        url "https://github.com/gobuffalo/buffalo/releases/download/v0.16.14/buffalo_0.16.14_Linux_armv6.tar.gz"
+        sha256 "78943b7617bf9fda7a43dab1248fc6ed33bdcfde4d6d409cc10b7ff9d00e8335"
       end
     end
   end
