54d000046f31a137a4c62ebaf5b71b9fbb2d699b
goreleaserbot <goreleaser@carlosbecker.com>
2020-11-10 18:25:13 +0100

    Brew formula update for packr version v2.8.1

--- packr.rb
+++ packr.rb
@@ -2,16 +2,16 @@
 class Packr < Formula
   desc ""
   homepage ""
-  version "2.7.1"
+  version "2.8.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/gobuffalo/packr/releases/download/v2.7.1/packr_2.7.1_darwin_amd64.tar.gz"
-    sha256 "2cb79a2e8d59d44c1057538442010ef7bac5110506df5644635cf533b3bb4b79"
+    url "https://github.com/gobuffalo/packr/releases/download/v2.8.1/packr_2.8.1_darwin_amd64.tar.gz"
+    sha256 "b0b512886708925923f6c78e462e7a31ecc90dd6aed17bd92490d24cc469c2a7"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/gobuffalo/packr/releases/download/v2.7.1/packr_2.7.1_linux_amd64.tar.gz"
-      sha256 "165a2e862be7255a8c7bbe5ba6d7e9123d945b83a71267197799013d89c2a739"
+      url "https://github.com/gobuffalo/packr/releases/download/v2.8.1/packr_2.8.1_linux_amd64.tar.gz"
+      sha256 "1cb2a0113550bc7962a8fda31a29877fcbbabd56b46c25e1fffbc225334162e7"
     end
   end
 
